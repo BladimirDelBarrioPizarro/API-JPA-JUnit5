f39db555d392db940eabda8dca022c061d031328
Bladi <bladimaltego@gmail.com>
2021-11-25 09:49:44 +0100

    feat: added BeforeEach and AfterEach tags

--- src/test/java/com/example/demo/model/entity/AccountTest.java
+++ src/test/java/com/example/demo/model/entity/AccountTest.java
@@ -2,36 +2,48 @@ package com.example.demo.model.entity;
 
 import com.example.demo.model.entity.dummy.AccountDummy;
 import com.example.demo.model.exceptions.HandleExceptionDevit;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class AccountTest {
+    Account account1;
+    Account account2;
+
+    @BeforeEach
+    void initTest(){
+      this.account1 =  AccountDummy.accountDummy1();
+      this.account2 = AccountDummy.accountDummy2();
+      log.info("Init test");
+    }
+
+    @AfterEach
+    void endTest(){
+        log.info("End test");
+    }
 
     @Test
     @DisplayName("Test name Account")
     void testNameAccount() {
-        Account account = AccountDummy.accountDummy1();
         String expected = "Bladimir";
-        String reality = account.getPerson();
+        String reality = account1.getPerson();
         // Se crea la instancia del string no es recomendable por el consumo de recursos
-        assertNotNull(account,"The account cannot be null");
+        assertNotNull(account1,"The account cannot be null");
         // Se recomienda usar una lambda
         assertEquals(expected,reality, () -> "The account name is not what expected: Expected: "+expected);
-        assertTrue(account.getPerson().equals(expected));
+        assertTrue(account1.getPerson().equals(expected));
     }
 
     @Test
     @DisplayName("Test balance Account")
     void testBalanceAccount() {
-        Account account = AccountDummy.accountDummy1();
-        assertEquals(1000.434,account.getBalance().doubleValue());
-        assertFalse(account.getBalance().compareTo(BigDecimal.ZERO) < 0);
-        assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) > 0);
+        assertEquals(1000.434,account1.getBalance().doubleValue());
+        assertFalse(account1.getBalance().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(account1.getBalance().compareTo(BigDecimal.ZERO) > 0);
     }
 
     @Test
@@ -48,29 +60,26 @@ class AccountTest {
     @DisplayName("Test method setSumDevit Account")
     void testSetSumDebitAccount() {
         // fail(); // Con fail fozamos el fallo del método
-        Account account1 =  AccountDummy.accountDummy2();
-        account1.setSumDevit(new BigDecimal("1.100"));
-        assertNotNull(account1.getBalance());
-        assertEquals("2.200",account1.getBalance().toString());
-        assertEquals(2.0,account1.getBalance().intValue());
+        this.account2.setSumDevit(new BigDecimal("1.100"));
+        assertNotNull(this.account2.getBalance());
+        assertEquals("2.200",account2.getBalance().toString());
+        assertEquals(2.0,account2.getBalance().intValue());
     }
 
     @Test
     @DisplayName("Test method setSubstractDevit Account")
     void testSubstractDevitAccount() {
-        Account account1 =  AccountDummy.accountDummy2();
-        account1.setSubstractDevit(new BigDecimal("1.100"));
-        assertNotNull(account1.getBalance());
-        assertEquals("0.000",account1.getBalance().toPlainString());
-        assertEquals(0.0,account1.getBalance().intValue());
+        this.account2.setSubstractDevit(new BigDecimal("1.100"));
+        assertNotNull(this.account2.getBalance());
+        assertEquals("0.000",this.account2.getBalance().toPlainString());
+        assertEquals(0.0,this.account2.getBalance().intValue());
     }
 
     @Test
     @DisplayName("Test exception HandleExceptionDevit Account")
     void testHandleExceptionDevitAccount() {
-        Account account1 =  AccountDummy.accountDummy2();
         Exception exception = assertThrows(HandleExceptionDevit.class, () -> {
-           account1.setSubstractDevit(new BigDecimal("2.200"));
+           this.account2.setSubstractDevit(new BigDecimal("2.200"));
         });
         String message = exception.getMessage();
         String expected = "Insufficient Money";
