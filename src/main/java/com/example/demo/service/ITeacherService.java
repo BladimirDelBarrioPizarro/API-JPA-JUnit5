f810f49ce9f4a8632789be8a1af24fe1249a5259
Bladi <bladimaltego@gmail.com>
2021-11-30 12:48:16 +0100

    feat: added method saveTeacher in class TeacherServiceImplTest

--- src/main/java/com/example/demo/service/ITeacherService.java
+++ src/main/java/com/example/demo/service/ITeacherService.java
@@ -8,8 +8,8 @@ import java.util.List;
 public interface ITeacherService {
     List<TeacherDTO> findAll() throws Exception;
     TeacherDTO findById(Long id) throws Exception;
-    TeacherDTO saveTeacher(Teacher child) throws Exception;
-    TeacherDTO updateTeacher(Teacher child) throws Exception;
+    TeacherDTO saveTeacher(Teacher teacher) throws Exception;
+    TeacherDTO updateTeacher(Teacher teacher) throws Exception;
     void deleteTeacher(Long id) throws Exception;
     Teacher findByIdSQL(Long id) throws Exception;
 }
--- src/test/java/com/example/demo/service/impl/TeacherServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/TeacherServiceImplTest.java
@@ -4,6 +4,7 @@ package com.example.demo.service.impl;
 import com.example.demo.dao.ITeacherDao;
 import com.example.demo.model.dto.TeacherDTO;
 import com.example.demo.model.entity.Teacher;
+import com.example.demo.model.mapper.TeacherMapper;
 import com.example.demo.service.ITeacherService;
 import com.example.demo.service.impl.dummy.TeacherDummy;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +13,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -43,9 +47,12 @@ class TeacherServiceImplTest {
 
     @Test
     void findAll() throws Exception {
+        // Given
         List<TeacherDTO> teacherDTOList = TeacherDummy.teacherDTOListDummy();
         when(teacherService.findAll()).thenReturn(teacherDTOList);
+        // When
         List<TeacherDTO> teacherDTOS = teacherService.findAll();
+        // Then
         assertNotNull(teacherDTOS);
         assertEquals(1,teacherDTOS.size());
         assertEquals(1L,teacherDTOS.get(0).getId());
@@ -56,9 +63,12 @@ class TeacherServiceImplTest {
 
     @Test
     void findById() throws Exception {
+        // Given
         TeacherDTO teacherDTO = TeacherDummy.teacherDTODummy();
         when(teacherService.findById(anyLong())).thenReturn(teacherDTO);
+        // When
         TeacherDTO teacherDTOResponse = teacherService.findById(1L);
+        // Then
         assertEquals("email@talento.com",teacherDTOResponse.getEmail());
         assertEquals(1L,teacherDTOResponse.getId());
         assertTrue(teacherDTOResponse.getName().contains("name"));
@@ -66,7 +76,28 @@ class TeacherServiceImplTest {
     }
 
     @Test
-    void saveTeacher() {
+    void saveTeacher() throws Exception {
+        // Given
+
+        // when(teacherService.saveTeacher(any(Teacher.class))).thenReturn(TeacherDummy.teacherDTODummy());
+        when(teacherService.saveTeacher(any(Teacher.class))).then(new Answer<TeacherDTO>(){
+
+            Long secuency = 1L;
+            @Override
+            public TeacherDTO answer(InvocationOnMock invocationOnMock) throws Throwable {
+                Teacher teacher = invocationOnMock.getArgument(0);
+                log.info(teacher.toString());
+                teacher.setId(secuency);
+                return TeacherMapper.mapTeacherToTeacherDTO(teacher);
+            }
+        });
+        // When
+        TeacherDTO teacherDTO = teacherService.saveTeacher(TeacherDummy.teacherDummy());
+        // Then
+        assertNotNull(teacherDTO.getId());
+        assertEquals(1L,teacherDTO.getId());
+        assertEquals("http://test",teacherDTO.getPhoto());
+        verify(teacherService).saveTeacher(any(Teacher.class));
     }
 
     @Test
